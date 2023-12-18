a02612c6173e70fa2be5f22f18a1ba633564c3d4
Thoreex <14024927+Thoreex@users.noreply.github.com>
2023-12-18 03:22:01 -0600

    bump addon version

--- OneForAll.lua
+++ OneForAll.lua
@@ -1,6 +1,6 @@
 OneForAll_Name = "OneForAll"
 OneForAll_FormalName = "One For All"
-OneForAll_Version = "v1.2.0"
+OneForAll_Version = "v1.2.1"
 OneForAll_ButtonPrefix = "LibDBIcon10_"
 OneForAll_Events = {}
 OneForAll_Callbacks = {}
